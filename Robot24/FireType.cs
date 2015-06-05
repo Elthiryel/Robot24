402c9563a9042d45b0386b60795e677ae0677aa8
Lisu <tennlos@gmail.com>
2015-06-05 16:52:38 +0200

    add enums

---
+++ Robot24/FireType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot24
+{
+    public enum FireType
+    {
+        NoFire = 0,
+        SmartFire = 1,
+        LightFire = 2,
+        HeavyFire = 3
+    }
+}
---
+++ Robot24/MoveType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot24
+{
+    public enum MoveType
+    {
+        Straight = 0,
+        Evade = 1,
+        Circle = 2
+    }
+}
