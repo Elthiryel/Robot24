b803bc5df53c2af24f3e39d03846fcb6507c4fb6
Elthiryel <piti.pco@gmail.com>
2015-06-06 17:58:15 +0200

    Implemented missing part of PassedRequirements.

--- Robot24/Config/Configuration.cs
+++ Robot24/Config/Configuration.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Robocode;
 
@@ -26,6 +23,16 @@ namespace Robot24.Config
 
         public bool PassedRequirements(ScannedRobotEvent lastRobotInfo, Robot ourRobot)
         {
+            var enemyDirectionRelativeToRobot = lastRobotInfo.Heading - ourRobot.Heading;
+            if (enemyDirectionRelativeToRobot > 180)
+                enemyDirectionRelativeToRobot -= 360;
+            if (enemyDirectionRelativeToRobot < -180)
+                enemyDirectionRelativeToRobot += 360;
+            var enemyDirectionRelativeToGun = lastRobotInfo.Heading - ourRobot.GunHeading;
+            if (enemyDirectionRelativeToGun > 180)
+                enemyDirectionRelativeToGun -= 360;
+            if (enemyDirectionRelativeToGun < -180)
+                enemyDirectionRelativeToGun += 360;
             if (Requirements.EnemyEnergy != null && !(lastRobotInfo.Energy >= Requirements.EnemyEnergy.Min && lastRobotInfo.Energy <= Requirements.EnemyEnergy.Max))
                 return false;
             if (Requirements.Energy != null && !(ourRobot.Energy >= Requirements.Energy.Min && ourRobot.Energy <= Requirements.Energy.Max))
@@ -34,9 +41,10 @@ namespace Robot24.Config
                 return false;
             if (Requirements.NumberOfPlayers != null && !(ourRobot.Others >= Requirements.NumberOfPlayers.Min && ourRobot.Others <= Requirements.NumberOfPlayers.Max))
                 return false;
-            ///todo:
-            /// 
-            /// 
+            if (Requirements.EnemyDirectionRelativeToRobot != null && !(enemyDirectionRelativeToRobot >= Requirements.EnemyDirectionRelativeToRobot.Min && enemyDirectionRelativeToRobot <= Requirements.EnemyDirectionRelativeToRobot.Max))
+                return false;
+            if (Requirements.EnemyDirectionRelativeToGun != null && !(enemyDirectionRelativeToGun >= Requirements.EnemyDirectionRelativeToGun.Min && enemyDirectionRelativeToGun <= Requirements.EnemyDirectionRelativeToGun.Max))
+                return false;
             return true;
         }
     }
